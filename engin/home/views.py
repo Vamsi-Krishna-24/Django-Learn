9f189a09f1ee05a1a87e0be80832cd8a08015372
vamsi krishna <iamvkrishna24@gmail.com>
2025-05-29 12:09:49 +0530

    Just a login page with no DB

---
+++ engin/home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+#Home view
+def home(request):
+    return render(request, 'home/home.html')
+
+#Login view
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request,'home/login.html',{'error': 'Invalid username or password'})
+    return render(request, 'home/login.html')
+
